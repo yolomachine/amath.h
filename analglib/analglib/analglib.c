939d7ea6b3eea6664cbbb14c6ca11f55e9c1101c
Alex <thisisbanana@hotmail.com>
2016-05-20 06:03:01 +1000

    Matrices
    Minor changes // refactored

--- analglib/analglib/analglib.c
+++ analglib/analglib/analglib.c
@@ -1,69 +1,91 @@
-#include "analglib.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "analglib.h"
+
+#define IS_NULL_VECTOR(a, b)\
+	((a->size == 1 && a->coords[0] == 0.0 && b->size != 0) ||\
+     (b->size == 1 && b->coords[0] == 0.0 && a->size != 0))
+
+#define VEC_LOCAL_INIT(sz)\
+	pvector res = (pvector)malloc(sizeof(vector));\
+	res->size = sz;\
+	res->coords = calloc(res->size, sizeof(double));\
 
 #define VEC_OP(v1, op, v2){\
-	pvector res = (pvector)malloc(sizeof(vector));\
-	res->size = v1->size;\
-	vecdata tmp = calloc(res->size, sizeof(vector));\
 	for (size i = 0; i < res->size; ++i)\
-		tmp[i] = v1->coords[i] op## v2->coords[i];\
-	res->coords = tmp;\
+		res->coords[i] = v1->coords[i] op## v2->coords[i];\
 	return res;\
 }
 
 #define VEC_SCALAR_OP(v, op, sc){\
-	pvector res = (pvector)malloc(sizeof(vector));\
-	res->size = v->size;\
-	vecdata tmp = calloc(res->size, sizeof(vector));\
 	for (size i = 0; i < res->size; ++i)\
-		tmp[i] = v->coords[i] op## sc;\
-	res->coords = tmp;\
+		res->coords[i] = v->coords[i] op## sc;\
 	return res;\
 }
 
-#define GET_SIZE(a) (a == NULL) ? -1 : a->size
+#define MX_LOCAL_INIT(sz, rows, cols)\
+	pmatrix res = (pmatrix)malloc(sizeof(matrix));\
+	res->size = sz;\
+	res->rowCount = rows;\
+	res->colCount = cols;\
+	res->cells = (mxdata)malloc(sizeof(vecdata));\
+	for (size i = 0; i < res->rowCount; ++i)\
+		res->cells[i] = calloc(res->colCount, sizeof(double));\
 
-pvector createVector(vecdata v, size sz){
-	if (v == NULL) return NULL;
-	pvector a = (pvector)malloc(sizeof(vector));
-	vecdata tmp = calloc(sz, sizeof(double));
-	for (size i = 0; i < sz; ++i)
-		tmp[i] = v[i];
-	a->coords = tmp;
-	a->size   = sz;
-	return a;
+#define MX_OP(mx1, op, mx2){\
+	for (size i = 0; i < res->rowCount; ++i)\
+		for (size j = 0; j < res->colCount; ++j)\
+			res->cells[i][j] = mx1->cells[i][j] op## mx2->cells[i][j];\
+	return res;\
 }
 
-void deleteVector(pvector a){
-	if (a == NULL) return;
-	free(a->coords);
-	free(a);
+#define MX_SCALAR_OP(mx, op, sc){\
+	for (size i = 0; i < res->rowCount; ++i)\
+		for (size j = 0; j < res->colCount; ++j)\
+			res->cells[i][j] = mx->cells[i][j] op## sc;\
+	return res;\
+}
+
+pvector createVector(vecdata v, size sz){
+	if (v == NULL || sz == 0) return NULL;
+	VEC_LOCAL_INIT(sz)
+	for (size i = 0; i < sz; ++i)
+		res->coords[i] = v[i];
+	return res;
+}
+
+void destroyVector(pvector v){
+	if (v == NULL) return;
+	free(v->coords);
+	free(v);
 }
 
 pvector addVector(pvector a, pvector b){
-	if (a != NULL && b != NULL && a->size == b->size) {
-		VEC_OP(a, +, b);
+	if (a != NULL && b != NULL && a->size == b->size && a->size != 0) {
+		VEC_LOCAL_INIT(a->size)
+		VEC_OP(a, +, b)
 	}
 	else return NULL;
 }
 
 pvector subVector(pvector a, pvector b){
-	if (a != NULL && b != NULL && a->size == b->size) {
-		VEC_OP(a, -, b);
+	if (a != NULL && b != NULL && a->size == b->size && a->size != 0) {
+		VEC_LOCAL_INIT(a->size)
+		VEC_OP(a, -, b)
 	}
 	else return NULL;
 }
 
 pvector mulVector(pvector a, double scalar){
-	if (a!= NULL && a->size != 0) {
-		VEC_SCALAR_OP(a, *, scalar);
+	if (a != NULL && a->size != 0) {
+		VEC_LOCAL_INIT(a->size)
+		VEC_SCALAR_OP(a, *, scalar)
 	}
 	else return NULL;
 }
 
 double dotVectors(pvector a, pvector b){
-	if (a != NULL && b != NULL && a->size == b->size) {
+	if (a != NULL && b != NULL && ((a->size == b->size && a->size != 0) || IS_NULL_VECTOR(a, b))) {
 		double scalar = 0.0;
 		for (size i = 0; i < a->size; ++i)
 			scalar += a->coords[i] * b->coords[i];
@@ -74,13 +96,91 @@ double dotVectors(pvector a, pvector b){
 
 pvector crossVectors(pvector a, pvector b){
 	if (a != NULL && b != NULL && a->size == 3 && b->size == 3) {
-		pvector c = (pvector)malloc(sizeof(vector));
-		c->size = 3;	
-		c->coords = calloc(c->size, sizeof(double));
-		c->coords[0] =  a->coords[1] * b->coords[2] - b->coords[1] * a->coords[2];
-		c->coords[1] = -a->coords[0] * b->coords[2] + b->coords[0] * a->coords[2];
-		c->coords[2] =  a->coords[0] * b->coords[1] - b->coords[0] * a->coords[1];
-		return c;
+		pvector res = (pvector)malloc(sizeof(vector));
+		res->size = 3;	
+		res->coords = calloc(res->size, sizeof(double));
+		res->coords[0] =  a->coords[1] * b->coords[2] - b->coords[1] * a->coords[2];
+		res->coords[1] = -a->coords[0] * b->coords[2] + b->coords[0] * a->coords[2];
+		res->coords[2] =  a->coords[0] * b->coords[1] - b->coords[0] * a->coords[1];
+		if (res->coords[0] == res->coords[1] && res->coords[1] == res->coords[2]) {
+			pvector null_vec = (pvector)malloc(sizeof(vector));
+			null_vec->coords = calloc(1, sizeof(double));
+			null_vec->coords[0] = 0.0;
+			null_vec->size = 1;
+			free(res);
+			return null_vec;
+		}
+		return res;
 	}
 	else return NULL;
+}
+
+pmatrix createMatrix(mxdata mx, size rows, size cols){
+	if (mx == NULL || rows == 0 || cols == 0) return NULL;
+	MX_LOCAL_INIT(cols * rows, rows, cols)
+	for (size i = 0; i < rows; ++i) {
+		res->cells[i] = calloc(cols, sizeof(double));
+		for (size j = 0; j < cols; ++j)
+			res->cells[i][j] = mx[i][j];
+	}
+	return res;
+}
+
+void destroyMatrix(pmatrix mx){
+	if (mx == NULL) return;
+	for (size i = 0; i < mx->rowCount; ++i)
+		free(mx->cells[i]);
+	free(mx->cells);
+	free(mx);
+}
+
+pmatrix addMatrix(pmatrix a, pmatrix b){
+	if (a != NULL && b != NULL && a->size == b->size && a->size != 0) {
+		MX_LOCAL_INIT(a->size, a->rowCount, a->colCount)
+		MX_OP(a, +, b)
+	}
+	else return NULL;
+}
+
+pmatrix subMatrix(pmatrix a, pmatrix b){
+	if (a != NULL && b != NULL && a->size == b->size && a->size != 0) {
+		MX_LOCAL_INIT(a->size, a->rowCount, a->colCount)
+		MX_OP(a, -, b)
+	}
+	else return NULL;
+}
+
+pmatrix mulMatrix(pmatrix a, double scalar){
+	if (a != NULL && a->size != 0) {
+		MX_LOCAL_INIT(a->size, a->rowCount, a->colCount)
+		for (size i = 0; i < res->rowCount; ++i)
+			for (size j = 0; j < res->colCount; ++i)
+				res->cells[i][j] = a->cells[i][j];
+		MX_SCALAR_OP(a, *, scalar)
+	}
+	else return NULL;
+}
+
+pmatrix transpose(pmatrix mx){
+	if (mx != NULL && mx->rowCount == mx->colCount && mx->size != 0) {
+		MX_LOCAL_INIT(mx->size, mx->rowCount, mx->colCount)
+		for (size i = 0; i < res->rowCount; ++i)
+			for (size j = 0; j < res->colCount; ++j)
+				res->cells[j][i] = mx->cells[i][j];
+		return res;
+	}
+	else return NULL;
+}
+
+//not implemented yet
+double getDeterminant(pmatrix mx){
+	if (mx != NULL && mx->rowCount == mx->colCount && mx->size != 0) {
+		MX_LOCAL_INIT(mx->size, mx->rowCount, mx->colCount)
+		for (size i = 0; i < res->rowCount; ++i)
+			for (size j = 0; j < res->colCount; ++i)
+				res->cells[i][j] = mx->cells[i][j];
+		double det = 1.0;
+		return det;
+	}
+	else return -1.0;
 }
--- analglib/analglib/analglib.h
+++ analglib/analglib/analglib.h
@@ -5,42 +5,39 @@
 	extern "C" {
 	#endif
 
+	#define GET_SIZE(a) (a == NULL) ? -1 : a->size
+
 	typedef unsigned int size;
 	typedef double* vecdata;
 	typedef struct {
 		vecdata coords;
 		size size;
-	} vector;
-	typedef vector* pvector;
+	} vector, *pvector;
 	
-	vector* createVector(vecdata v, size sz);
-	vector* addVector(vector* a, vector* b);
-	vector* subVector(vector* a, vector* b);
-	vector* mulVector(vector* a, double scalar);
-	vector* crossVectors(vector* a, vector*b);
-	double  dotVectors(vector* a, vector* b);
-	void destroyVector(vector* a);
+	extern pvector createVector(vecdata v, size sz);
+	extern pvector addVector(pvector a, pvector b);
+	extern pvector subVector(pvector a, pvector b);
+	extern pvector mulVector(pvector a, double scalar);
+	extern pvector crossVectors(pvector a, pvector b);
+	extern double  dotVectors(pvector a, pvector b);
+	extern void destroyVector(pvector v);
 
 	typedef double** mxdata;
 	typedef struct {
 		mxdata cells;
 		size rowCount, colCount;
 		size size;
-	} matrix;
-	typedef matrix* pmatrix;
+	} matrix, *pmatrix;
 	
-	matrix* createMatrix(mxdata mx, size rows, size cols);
-	matrix* addMatrix(matrix* a, matrix* b);
-	matrix* subMatrix(matrix* a, matrix* b);
-	matrix* mulMatrix(matrix* a, double scalar);
-	matrix* transpose(matrix* a);
-	int getDeterminant(matrix* a);
+	extern pmatrix createMatrix(mxdata mx, size rows, size cols);
+	extern pmatrix addMatrix(pmatrix a, pmatrix b);
+	extern pmatrix subMatrix(pmatrix a, pmatrix b);
+	extern pmatrix mulMatrix(pmatrix a, double scalar);
+	extern pmatrix transpose(pmatrix mx);
+	extern double getDeterminant(pmatrix mx);
+	extern void destroyMatrix(pmatrix mx);
 
 	#ifdef __cplusplus
 	}
 	#endif
-#endif
-
-#ifndef _CRT_SECURE_NO_WARNINGS
-	#define _CRT_SECURE_NO_WARNINGS
 #endif
