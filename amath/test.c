7354f47c62bbafc3bab8742ba66dda45ffb3307c
yolomachine <thisisbanana@hotmail.com>
2016-06-02 23:26:13 +0900

    Update test.c

--- amath/test.c
+++ amath/test.c
@@ -9,20 +9,20 @@ int main(int argc, char* argv[]){
 
 	_createVector	  =  (__createVector)	GetProcAddress(hLib, "createVector");
 	_vectorOperation  =  (__vectorOperation)GetProcAddress(hLib, "vectorOperation");
-	_mulVector		  =  (__mulVector)		GetProcAddress(hLib, "mulVector");
+	_mulVector	  =  (__mulVector)	GetProcAddress(hLib, "mulVector");
 	_crossVectors	  =  (__crossVectors)	GetProcAddress(hLib, "crossVectors");
-	_dotVectors		  =  (__dotVectors)		GetProcAddress(hLib, "dotVectors");
+	_dotVectors	  =  (__dotVectors)	GetProcAddress(hLib, "dotVectors");
 	_destroyVector	  =  (__destroyVector)	GetProcAddress(hLib, "destroyVector");
-	_getVecData		  =  (__getVecData)		GetProcAddress(hLib, "getVecData");
-	_getVecSize		  =  (__getVecSize)		GetProcAddress(hLib, "getVecSize");
+	_getVecData	  =  (__getVecData)	GetProcAddress(hLib, "getVecData");
+	_getVecSize	  =  (__getVecSize)	GetProcAddress(hLib, "getVecSize");
 	_createMatrix	  =  (__createMatrix)	GetProcAddress(hLib, "createMatrix");
 	_matrixOperation  =  (__matrixOperation)GetProcAddress(hLib, "matrixOperation");
-	_mulMatrix		  =  (__mulMatrix)		GetProcAddress(hLib, "mulMatrix");
-	_transpose		  =  (__transpose)		GetProcAddress(hLib, "transpose");
+	_mulMatrix	  =  (__mulMatrix)	GetProcAddress(hLib, "mulMatrix");
+	_transpose	  =  (__transpose)	GetProcAddress(hLib, "transpose");
 	_getDeterminant	  =  (__getDeterminant)	GetProcAddress(hLib, "getDeterminant");
 	_destroyMatrix	  =  (__destroyMatrix)	GetProcAddress(hLib, "destroyMatrix");
-	_getMxData		  =  (__getMxData)		GetProcAddress(hLib, "getMxData");
-	_getMxSize		  =  (__getMxSize)		GetProcAddress(hLib, "getMxSize");
+	_getMxData	  =  (__getMxData)	GetProcAddress(hLib, "getMxData");
+	_getMxSize	  =  (__getMxSize)	GetProcAddress(hLib, "getMxSize");
 
 	return 0;
 }
